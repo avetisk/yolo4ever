ea7041ccdc2acee9e5b25df78ce6dd477fe5c0de
Alix BERTHELOT <alixberthelot03@gmail.com>
2022-06-30 09:52:26 +0200

    add 3

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ const IndexPage = () => {
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setCount(count + 3);
   };
 
   return (
     <>
       <header>{count}</header>
-      <button onClick={handleClick}>+1</button>
+      <button onClick={handleClick}>+3</button>
     </>
   );
 };
